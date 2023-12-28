7adde67682be28021060dc2d5687c793cfe1adf5
r2hu1 <87074472+r2hu1@users.noreply.github.com>
2023-12-28 16:16:30 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -7,8 +7,8 @@ export default async function Home() {
   return (
     <main>
       <Hero />
-      <EbookProducts />
       <TemplateProducts />
+      <EbookProducts />
       <IconProducts />
     </main>
   );
