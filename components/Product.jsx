dffe98de4879b01d3aedadacd9c69cafa811cd71
r2hu1 <newasram86@gmail.com>
2023-12-29 15:36:01 +0530

    product image fix 2

--- components/Product.jsx
+++ components/Product.jsx
@@ -6,7 +6,7 @@ export default function Product({ link, image, price, name }) {
     return (
         <div className="border rounded-md w-fit bg-background dark:bg-secondary p-1 transition hover:scale-105 h-full">
             <Link href={link}>
-                <img className="rounded-md md:h-[200px] md:w-[300px] w-full h-full min-h-[200px] min-w-[300px] max-h-[200px] max-w-[320px]" src={image} alt="thumbnail"></img>
+                <img className="rounded-md md:h-[200px] md:w-[300px] w-full h-full min-h-[200px] min-w-[320px] max-h-[200px] max-w-[320px]" src={image} alt="thumbnail"></img>
             </Link>
             <Link href={link}>
                 <div className="px-5 py-4 mt-1 flex justify-between items-center">
